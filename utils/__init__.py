bc3f8d246e00e70e08987f25b0ec1df90038392e
AZhou <xiaoxiangyafei_1@163.com>
2024-04-24 20:31:13 +0800

    add ema

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,9 +1,11 @@
 from .data_structure import SegDataSample
 from .local_visualizer import SegLocalVisualizer
 from .resize_longest_side import ResizeLongestSide
+from .ema import ExponentialMovingAverage
 from .losses import *
 from .tools import *
 
 __all__ = [
-    'SegDataSample', 'SegLocalVisualizer', 'ResizeLongestSide'
+    'SegDataSample', 'SegLocalVisualizer', 'ResizeLongestSide',
+    'ExponentialMovingAverage'
 ]
